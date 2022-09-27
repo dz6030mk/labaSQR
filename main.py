1642e37d7795e1ff601f47af1eb6c70a237800df
dz6030mk <kalinchevandrey@mail.ru>
2022-09-27 21:21:54 +0300

    homework code

---
+++ main.py
@@ -0,0 +1,14 @@
+a = float(input('Введите коэффициент a: '))
+b = float(input('Введите коэффициент b: '))
+c = float(input('Введите коэффициент c: '))
+discr = b**2 - 4*a*c
+if discr < 0:
+    print('Корней нет')
+elif discr == 0:
+    x = -b / (2 * a)
+    print('x = ' + str(x))
+else:
+    x1 = (-b + discr ** 0.5) / (2 * a)
+    x2 = (-b - discr ** 0.5) / (2 * a)
+    print('x1 = ' + str(x1))
+    print('x2 = ' + str(x2))
